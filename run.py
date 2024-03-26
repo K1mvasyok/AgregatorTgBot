7284e63112f0367d4fd81e7d6946973e4e669e7d
K1mvasyok <neancatvasia12@mail.ru>
2024-03-26 20:59:25 +0800

    start

---
+++ run.py
@@ -0,0 +1,17 @@
+import asyncio
+
+from aiogram import Bot, Dispatcher
+from config import TOKEN
+import asyncio
+
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher()
+
+
+async def main():
+    await dp.start_polling(bot)
+    
+    
+if __name__ == '__main__':
+    asyncio.run(main())
