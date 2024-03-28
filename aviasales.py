17e5963527e8db28e2b682c5fe68b2d3523051ce
K1mvasyok <neancatvasia12@mail.ru>
2024-03-28 12:43:32 +0800

    Update aviasales.py

--- aviasales.py
+++ aviasales.py
@@ -2,4 +2,4 @@ import requests
 
 from config import api, marker
 
-response = requests.get(f'https://{api}.travelpayouts.com/aviasales/v3/prices_for_dates')
+response = requests.get('https://api.travelpayouts.com/aviasales/v3/prices_for_dates')
